0c4583a9d50daac022a8f44ea8c3e1232a5f33a1
Jack Holdsworth <jackch90@gmail.com>
2020-05-10 21:33:44 +0100

    Mongoose user login details model

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
-const router = express.Router();
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000
+const passport = require('passport');
 
-app.listen(3000, () => {
-    console.log('Listening on port: ' + port);
-});
+// Middleware
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
+// Mongoose connection
+const url = process.env.MONGO_URL
+mongoose.connect(url, { useNewUrlParser: true})
+const db = mongoose.connection;
+db.on('error', (err) => {
+    console.log('Mongo connection error: ' + err);
+})
+db.once('open', () => {
+    console.log('MongoDB connected: ' + url);
+})
+
+// Mongoose - create user secret details model
+const Schema = mognoose.Schema;
+const UserPass = new Schema({
+    username: String,
+    password: String
+}) 
+
+UserPass.plugin(passportLocalMongoose);
+const userInfo = mongoose.model('userInfo', UserPass, 'userInfo')
+
+// Routing
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
+app.listen(port, () => {
+    console.log('Listening on port: ' + port);
+});
